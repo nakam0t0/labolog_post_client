76ba9b32fc9aec1bfd8d809871e0b5cbe2fd8781
Your Name <you@example.com>
2018-02-03 15:07:00 +0900

    arrange way to check monitor

--- monitorCheck.py
+++ monitorCheck.py
@@ -1,5 +1,6 @@
 # coding: utf-8
-import cv2, requests, numpy as np
+import cv2, numpy as np
+from datetime import datetime
 
 # 内蔵カメラを起動
 cap = cv2.VideoCapture(0)
@@ -8,12 +9,18 @@ cap = cv2.VideoCapture(0)
 img = np.zeros((480,640,3), np.uint8)
 
 # OpenCVに用意されている顔認識するためのxmlファイルのパス
+# cascade_path = "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
+# cascade_path = "/home/myjlab/.pyenv/versions/3.6.0/envs/labolog/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_alt.xml"
 cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
+eye_cascade_path = "/usr/share/opencv/haarcascades/haarcascade_eye.xml"
 # カスケード分類器の特徴量を取得する
 cascade = cv2.CascadeClassifier(cascade_path)
+eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
 
 # 顔に表示される枠の色を指定（白色）
 color = (255,255,255)
+# 顔画像のナンバリング用
+count = 0
 
 print('start')
 
@@ -35,7 +42,15 @@ try:
                 height = rect[3]
                 dst = frame[y:y+height, x:x+width]
 
-                print('face!')
+                eyes = eye_cascade.detectMultiScale(dst)
+                for (ex, ey, ew, eh) in eyes:
+                    # 顔画像を切り出して書き出し
+                    path = "check/" + str(datetime.now()) + "(" + str(count) + ").jpg"
+                    cv2.imwrite(path, frame)
+                    cv2.rectangle(dst, (ex, ey), (ex + ew, ey + eh), color, 2)
+
+                    print('face' + str(count))
+                    count += 1
 
         # 表示
         cv2.imshow("frame", frame)
